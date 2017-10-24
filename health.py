7050781addf54cdf6394f978c3cbb18ba7336057
Adrianne M. Padua <hello@adrimarie.me>
2017-10-23 21:26:29 -0700

    Health Potion random number generator with RANDOM module

---
+++ health.py
@@ -0,0 +1,8 @@
+# health potion (video game) random number generator script thing using the RANDOM module
+# check Python docs for the lists of modules to use
+
+import random
+
+health = 50
+
+potion_health = random.randint(25,50)
