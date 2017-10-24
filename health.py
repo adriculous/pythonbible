49e4c617d0acc810a929ee437af7ae29b47d50bd
Adrianne M. Padua <hello@adrimarie.me>
2017-10-23 21:39:36 -0700

    Finished script using variables, RANDOM modules, and casting

--- health.py
+++ health.py
@@ -5,4 +5,14 @@ import random
 
 health = 50
 
-potion_health = random.randint(25,50)
+# easy - 1, normal - 2, difficult - 3
+
+difficulty = 3
+
+# convert result into an integer (int) since division statements will appear with a 'float' data type
+
+potion_health = int(random.randint(25,50) / difficulty)
+
+health = health + potion_health
+
+print(health)
