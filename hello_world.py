ffeb20552a0d527663b1552f9a99f230e459a536
Adrianne M. Padua <hello@adrimarie.me>
2017-10-24 20:30:36 -0700

    That famous 'hello world' program

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# single quotes (') or double quotes (") are ok either way
+
+hello = 'Hello world!'
+
+print(hello)
+
+# use single quotes (') if your string includes a quote ('He said, " You're awesome! " ')
+
+# use triple quotes (''') if your string is a paragraph or long sentences. ('''blah blah blah... long text blah...''')
