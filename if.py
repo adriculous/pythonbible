6f573ecd217af251dc0005f0f01e4bc4dc247397
Adrianne M. Padua <hello@adrimarie.me>
2017-10-25 22:56:53 -0700

    Sample 'if-elif-else' statement exercise

---
+++ if.py
@@ -0,0 +1,15 @@
+# "if" statements, comparison operators, booleans stuff
+
+num1 = 120
+num2 = 120
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+elif num2 > num1:
+    print("num2 is bugger than num1")
+else:
+    print("both numbers are equal")
+
+# "If this is true, then do this. If it's not, do this instead. (If it's still not true,
+# do this, etc. etc.) Otherwise, they're both equal.
+# (and let it be known of this fact.)"
