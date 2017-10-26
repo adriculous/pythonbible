20db63456c1b15e8fee3a4d01255e5ed9bcd86e8
Adrianne M. Padua <hello@adrimarie.me>
2017-10-26 01:26:09 -0700

    Added logical operators examples

--- if.py
+++ if.py
@@ -13,3 +13,22 @@ else:
 # "If this is true, then do this. If it's not, do this instead. (If it's still not true,
 # do this, etc. etc.) Otherwise, they're both equal.
 # (and let it be known of this fact.)"
+
+# logical operators stuff
+
+A = 19
+B = 45
+
+if not A < B:
+    print("Well yeah, obviously not true.")
+
+
+if (A > 10 and B > 10):
+    print("Yep, both seem to be true.")
+
+
+if A > 10 or B < 10:
+    print("They're both true.")
+
+if not ((A > 10 and B > 10) or (A > 45 nd B > 45)):
+    print("Looks complicated, but true.")
