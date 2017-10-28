728785c2364f669fe38d66dd0a2031cfc4d10d1a
Adrianne M. Padua <hello@adrimarie.me>
2017-10-28 00:58:09 -0700

    Finished 'travis.py' script w/ '.remove' and '.append'

--- travis.py
+++ travis.py
@@ -1,13 +1,25 @@
 # list of known users here
 
 known_users = ["Alice", "Bob", "Claire", "Dan", "Emma", "Fred", "Georgie", "Harry"]
-print(len(known_users))
 
 while True:
     print("Hi! My name is Travis.")
     name = input("What is your name?: ").strip().capitalize()
 
     if name in known_users:
-        print("Name recognized")
+        print("Hello {}!".format(name))
+        remove = input("Would you like to be removed from the system (y/n)? ")
+
+        if remove == "y":
+            known_users.remove(name)
+        elif remove == "n":
+            print("No problem, I didn't want you to leave anyway!")
+
     else:
-        print("Name NOT recognized")
+        print("Hmmm... I don't think I have met you yet {}.".format(name))
+        add_me = input("Would you like to be added to the system (y/n)?: ").strip().lower()
+        if add_me == "y":
+            known_users.append(name)
+        elif add_me == "n":
+            print("No worries, see you around!")
+            
